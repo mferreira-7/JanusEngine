dcd64d73f9f876b8786d59934a97f406d00bf8f4
Ferreira <mf22528@essex.ac.uk>
2024-11-17 05:33:12 +0000

    chess board created

--- src/Chess/ChessMain.py
+++ src/Chess/ChessMain.py
@@ -2,7 +2,7 @@
 Handles user input and displays the current game state
 """
 
-import pygame as p
+import pygame as pg
 import ChessEngine
 
 WIDTH = HEIGHT = 512  #this could be 400
@@ -15,7 +15,71 @@ IMAGES = {}
 Initialising the global dictionary to hold images, this will benefit performance
 """
 
+
 def loadImages():
     pieces = ["rB", "nB", "bB", "qB", "kB", "pB", "pW", "rW", "nW", "bW", "qW", "kW"]
     for piece in pieces:
-        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE)) #IMAGES["pB"] will return the black pawn image
+        IMAGES[piece] = pg.transform.scale(pg.image.load("images/" + piece + ".png"),
+                                           (SQ_SIZE, SQ_SIZE))  #IMAGES["pB"] will return the black pawn image
+
+
+"""
+main function for handling user input and updating the graphics
+"""
+
+
+def main():
+    pg.init()
+    screen = pg.display.set_mode((WIDTH, HEIGHT))
+    clock = pg.time.Clock()
+    screen.fill(pg.Color('white'))
+    gameState = ChessEngine.GameState()
+    loadImages()  #only onces
+    running = True
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+        drawGameState(screen, gameState)
+        clock.tick(MAX_FPS)
+        pg.display.flip()
+
+
+"""
+Function to handle all the graphics of this program
+"""
+
+
+def drawGameState(screen, gameState):
+    drawBoard(screen)  #draw squares on the board
+    drawPieces(screen, gameState.board)  #draw pieces on the board
+
+
+"""
+Draw the squares
+"""
+
+
+def drawBoard(screen):
+    colors = [pg.Color("white"), pg.Color("grey")]
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            color = colors[((row + column) % 2)]
+            pg.draw.rect(screen, color, pg.Rect(column * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+"""
+Draw the pieces on the board using the current GameState.board
+"""
+
+
+def drawPieces(screen, board):
+    for row in range(DIMENSION):
+        for column in range(DIMENSION):
+            piece = board[row][column]
+            if piece != "--":
+                screen.blit(IMAGES[piece], pg.Rect(column * SQ_SIZE, row * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == '__main__':
+    main()
